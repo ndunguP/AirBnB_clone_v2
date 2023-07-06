52e49762cc7e08441e0de6cc4ade0c5b2983b484
ndunguP <peter.ndunguoz50@gmail.com>
2023-07-06 15:55:28 -0700

    initial commit

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,24 +1,91 @@
+#!/usr/bin/python3
+'''deletes out-of-date archives, using the function do_clean'''
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
+
+
+env.hosts = ['34.239.147.107', '52.3.247.207']
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
+
+
 def do_clean(number=0):
-  """Deletes out-of-date archives.
-  number is the number of the archives, including the most recent, to keep. If
-  number is 0 or 1, keep only the most recent version of your archive. if
-  number is 2, keep the most recent, and second most recent versions of your
-  archive. etc.
-  Args:
-    number: The number of archives to keep.
-  """
-  # Delete all unnecessary archives (all archives minus the number to keep) in the
-  # versions folder.
-  with cd('versions'):
-    for archive in listdir('.'):
-      if archive.endswith('.tar.gz'):
-        if int(archive.split('-')[-1][:-7]) < number:
-          delete(archive)
-  # Delete all unnecessary archives (all archives minus the number to keep) in the
-  # /data/web_static/releases folder of both of your web servers.
-  for host in ['web1', 'web2']:
-    with cd('/data/web_static/releases'):
-      for archive in listdir('.'):
-        if archive.endswith('.tar.gz'):
-          if int(archive.split('-')[-1][:-7]) < number:
-            sudo('rm -f {}'.format(archive))
+    """Deletes out-of-date archives of the static files.
+    Args:
+        number (Any): The number of archives to keep.
+    """
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
